238b87bb11034e570cd58a6620a662cdb84f036d
mrreikol <raazovmustafa@gmail.com>
2026-05-20 13:48:34 +0500

    Delete logger.py

--- logger.py
+++
@@ -1,9 +0,0 @@
-def log_action(func):
-
-    def wrapper(*args, **kwargs):
-        print("[LOG] Running:", func.__name__)
-        result = func(*args, **kwargs)
-        print("[LOG] Finished:", func.__name__)
-        return result
-
-    return wrapper
